9875ae87217c80e1458845eeb077c12eca4555f3
HamedAyani <hamedayani@gmail.com>
2022-03-10 10:53:27 +0700

    menambahkan file hello.php

---
+++ hello.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+<body>    
+    <h1>Profil</h1>
+    <?php
+    echo "Nama: Irham Hamed Ayani <br>";
+    echo "NIM : 200411100114";
+    ?>
+</body>
+</html>
