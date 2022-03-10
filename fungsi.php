c83645d9e94ac0c355bd037268c268ea5ae5aa5a
HamedAyani <hamedayani@gmail.com>
2022-03-10 10:54:25 +0700

    menambahkan file fungsi.php

---
+++ fungsi.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fungsi</title>
+</head>
+<body>
+    <h1>Fungsi</h1>
+<?php
+function add(int $x, int $y) {
+    $hasiladd = $x + $y;
+    return $x.' + '.$y.' = '.$hasiladd;
+    }
+
+echo add(12, 11) . "<br>";
+echo add(14, 15) . "<br>";
+echo add(5, 7);
+?>
+</body>
+</html>
